afd7ebe8ac700f719e03116a0880aaee062270cf
Vitor Brangioni <vitorh.brangioni@gmail.com>
2020-11-18 12:51:15 -0300

    Done login and logout using context

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Button} from 'react-native';
 
-const Dashboard: React.FC = () => <View />;
+import AuthContext from '../../contexts/auth';
+
+const Dashboard: React.FC = () => {
+  const {signOut} = useContext(AuthContext);
+
+  function handleSignOut() {
+    signOut();
+  }
+
+  return (
+    <View>
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  );
+};
 
 export default Dashboard;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import {View, Button} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Button, StyleSheet} from 'react-native';
+import AuthContext from '../../contexts/auth';
 
-const SignIn: React.FC = () => (
-  <View>
-    <Button title="Sign in" onPress={() => {}} />
-  </View>
-);
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center'},
+});
+
+const SignIn: React.FC = () => {
+  const {signed, user, signIn} = useContext(AuthContext);
+  console.log(signed, user);
+
+  function handleSignIn() {
+    signIn();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign in" onPress={handleSignIn} />
+    </View>
+  );
+};
 
 export default SignIn;
